5005a8e8a778d5294872e3f56f8ab061401cb5b0
tony <wabslygzj@163.com>
2019-07-25 23:46:11 +0800

    commit

--- src/main/java/com/tony/hbase/util/HbaseRowKeyUtils.java
+++ src/main/java/com/tony/hbase/util/HbaseRowKeyUtils.java
@@ -17,19 +17,23 @@ public class HbaseRowKeyUtils {
 
     public static byte[] getAcctRowKey(long account, String channel, long timestamp, long serialNo) throws Exception {
         ByteBuffer keyBuffer = ByteBuffer.allocate(28);
-        keyBuffer.put(Objects.requireNonNull(TonyHbaseConf.getIdx(account)))
+        keyBuffer
+                .put(Objects.requireNonNull(TonyHbaseConf.getIdx(account)))
                 .put(channel.getBytes()).putLong(account).putLong(timestamp)
                 .putLong(serialNo);
+
         return keyBuffer.array();
     }
 
     public static byte[] getIdRowKey(String id, String channel, long timestamp, long serialNo) {
         ByteBuffer byteBuffer = ByteBuffer.allocate(38);
-        byteBuffer.put(TonyHbaseConf.getIdx(id))
+        byteBuffer
+                .put(TonyHbaseConf.getIdx(id))
                 .put(channel.getBytes())
                 .put(id.getBytes())
                 .putLong(timestamp)
                 .putLong(serialNo);
+
         return byteBuffer.array();
     }
 
@@ -39,24 +43,35 @@ public class HbaseRowKeyUtils {
                 .put(channel.getBytes())
                 .putLong(timestamp)
                 .putLong(serialNo);
+
         return buffer.array();
     }
 
     public static byte[] getTimeRowKey(long serialNo, String channel, long timestamp) {
         ByteBuffer byteBuffer = ByteBuffer.allocate(20);
-        byteBuffer.put(Objects.requireNonNull(TonyHbaseConf.getIdx(serialNo))).put(channel.getBytes()).putLong(timestamp);
+        byteBuffer
+                .put(Objects.requireNonNull(TonyHbaseConf.getIdx(serialNo)))
+                .put(channel.getBytes()).putLong(timestamp);
         return byteBuffer.array();
     }
 
     public static byte[] getIdxRowKey(long serialNo) {
         ByteBuffer keyBuffer = ByteBuffer.allocate(10);
-        keyBuffer.put(Objects.requireNonNull(TonyHbaseConf.getIdx(serialNo))).putLong(serialNo);
+        keyBuffer
+                .put(Objects.requireNonNull(TonyHbaseConf.getIdx(serialNo)))
+                .putLong(serialNo);
+
         return keyBuffer.array();
     }
 
     public static byte[] getRegionRowkey(String region, String channel, long timestamp, long serialNo) {
         ByteBuffer keyBuffer = ByteBuffer.allocate(20);
-        keyBuffer.put(Objects.requireNonNull(TonyHbaseConf.getIdx(serialNo))).put(channel.getBytes()).putLong(timestamp).putLong(serialNo);
+        keyBuffer
+                .put(Objects.requireNonNull(TonyHbaseConf.getIdx(serialNo)))
+                .put(channel.getBytes())
+                .putLong(timestamp)
+                .putLong(serialNo);
+
         return keyBuffer.array();
     }
 }
