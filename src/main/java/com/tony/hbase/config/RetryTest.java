a563e63bf443da3f08f3680d0f695073489f64b0
tony <wabslygzj@163.com>
2019-07-29 12:36:51 +0800

    commit

--- src/main/java/com/tony/hbase/config/RetryTest.java
+++ src/main/java/com/tony/hbase/config/RetryTest.java
@@ -13,16 +13,18 @@ import java.util.Random;
 public class RetryTest {
     public static int RETRY_BACKOFF[] = {1, 2, 3, 5, 10, 20, 40, 100, 100, 100, 100, 200, 200};
 
-    public static  long retry(int pause, int ntries) {
+    public static long retry(int pause, int ntries) {
+        //这里可以看出来,可以优化成到达一定重试次数再执行到下一个
         long normalPause = pause * RETRY_BACKOFF[ntries];
+        //这里添加了抖动,防止使键分布更加均匀
         long jitter = (long) (normalPause * RandomUtils.nextFloat() * 0.01f);
         System.out.println(jitter);
         return normalPause + jitter;
     }
 
     public static void main(String[] args) {
-        for(int i=0;i<50;i++){
-            retry(10,6);
+        for (int i = 0; i < 50; i++) {
+            retry(10, 6);
         }
     }
 }
