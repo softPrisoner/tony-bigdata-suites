a0e74e7f9ee2068e2142e9f3b299c7488410c389
tony <wabslygzj@163.com>
2019-08-03 07:11:40 +0800

    transaction   biz

---
+++ src/main/java/com/tony/hbase/spark/TransactionSpark.scala
@@ -0,0 +1,22 @@
+package com.tony.hbase.spark
+
+/**
+  * @author tony
+  * @describe TransactionSpark
+  * @date 2019-08-02
+  */
+
+class TransactionSpark {
+  def pullRecordFromHDFS(): Unit = {
+    //
+
+  }
+
+  def computeWithRecord(): Unit = {
+
+  }
+
+  def writeHbase(): Unit = {
+
+  }
+}
