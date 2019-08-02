541da58ef77d3810ee2219991dd81e815ded9104
tony <wabslygzj@163.com>
2019-08-02 15:04:57 +0800

    demo for hbase

--- src/test/java/com/elianda/etest/HbaseConnection.java
+++ src/test/java/com/elianda/etest/HbaseConnection.java
@@ -1,12 +1,9 @@
 package com.elianda.etest;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HRegionInfo;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -32,7 +29,47 @@ public class HbaseConnection {
         try {
             connection = ConnectionFactory.createConnection(hbaseConf);
             Admin admin = connection.getAdmin();
-            final List<HRegionInfo> test = admin.getTableRegions(TableName.valueOf("test"));
+            if (!admin.tableExists(TableName.valueOf("TRANSACTION"))) {
+                HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf("TRANSACTION"));
+                String channelID = "ThirdPay";
+                HColumnDescriptor columnDescriptor = new HColumnDescriptor(channelID);
+                tableDescriptor.addFamily(columnDescriptor);
+                admin.createTable(tableDescriptor);
+            } else {
+                //防止表被禁止为不可用
+                if (admin.isTableDisabled(TableName.valueOf("test"))) {
+                    admin.isTableAvailable(TableName.valueOf("test"));
+                }
+                String channelID = "ThirdPay";
+                String merchantNumber = "1000001";
+                String transactionNumber = "5000";
+                Table transactionTable = connection.getTable(TableName.valueOf("TRANSACTION"));
+                Put put = new Put(Bytes.toBytes("testRow1"));
+                put.addColumn(Bytes.toBytes(channelID),
+                        Bytes.toBytes(merchantNumber)
+                        , Bytes.toBytes(transactionNumber));
+//                transactionTable.put(put);
+                //单条记录
+                Get get = new Get(Bytes.toBytes("testRow1"));
+                get.addColumn(Bytes.toBytes(channelID), Bytes.toBytes(merchantNumber));
+                Result result = transactionTable.get(get);
+                List<Cell> cells = result.listCells();
+                for (Cell cell : cells) {
+                    System.out.println
+                            ("列簇名:" + Bytes.toString(cell.getFamilyArray()
+                                    , cell.getFamilyOffset()
+                                    , cell.getFamilyLength()));
+                    System.out.println
+                            ("唯一标识" + Bytes.toString(cell.getQualifierArray()
+                                    , cell.getQualifierOffset()
+                                    , cell.getQualifierLength()));
+                    System.out.println
+                            ("存储的值:" + Bytes.toString(cell.getValueArray()
+                                    , cell.getValueOffset()
+                                    , cell.getValueLength()));
+                }
+
+            }
             System.out.println("获取HBase连接成功");
         } catch (IOException var5) {
             System.out.println("获取HBase连接失败" + var5.getMessage());
