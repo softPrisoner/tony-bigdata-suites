e2ffab6b6c35e8ac9addb5a1ee400b95eb028d8a
tony <wabslygzj@163.com>
2019-08-03 08:45:26 +0800

    file type hdfs prefix

---
+++ src/main/java/com/tony/hbase/contants/FilePrefixConstants.java
@@ -0,0 +1,13 @@
+package com.tony.hbase.contants;
+
+/**
+ * @author tony
+ * @describe FilePrefixConstants
+ * @date 2019-08-03
+ */
+public class FilePrefixConstants {
+    public static final String LOG_TYPE = "/log/";
+    public static final String TRANSACTION_TYPE = "/transaction/";
+    public static final String VEDIO_TYPE = "/video/";
+    public static final String AUDIO_TYPE = "/audio/";
+}
