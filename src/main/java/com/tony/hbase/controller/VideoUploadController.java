29e7fcba71962564bc2bc0035223c27d7f71963b
tony <wabslygzj@163.com>
2019-08-03 22:38:34 +0800

    modify add support nginx proxy

--- src/main/java/com/tony/hbase/controller/VideoUploadController.java
+++ src/main/java/com/tony/hbase/controller/VideoUploadController.java
@@ -4,9 +4,9 @@ import com.tony.hbase.hdfs.HDFSService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import java.io.IOException;
@@ -29,26 +29,27 @@ public class VideoUploadController {
         return "VedioUpload.html";
     }
 
+    //sudo apt-get install nginx-extras
     @RequestMapping("/page/play")
-    public String playPage() {
+    public String playPage(@RequestParam String filePath, Model model) throws IOException, InterruptedException {
+        String videoPath = hdfsService.downloadFile(filePath);
+        model.addAttribute("videoPath", videoPath);
         return "VedioPlay.html";
     }
 
     @PostMapping("/video/upload")
-    public String uploadVideo(@RequestParam String type, MultipartFile file) throws IOException {
+    public String uploadVideo(@RequestParam String type, MultipartFile file) throws IOException, InterruptedException {
         if (null == file || file.isEmpty()) {
             //前端可做路径校验
             return "文件不能为空";
         }
         String filePath = hdfsService.uploadFile(file, "video");
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("filePath", filePath);
         LOGGER.info("upload video success");
-        return "redirect:/page/play";
+        return "redirect:/v1/page/play?filePath=" + filePath;
     }
 
     @PutMapping("/audio/upload")
-    public String uploadAudio(@RequestBody MultipartFile file) throws IOException {
+    public String uploadAudio(@RequestBody MultipartFile file) throws IOException, InterruptedException {
         if (null == file || file.isEmpty()) {
             //前端可做路径校验
             return "文件不能为空";
