320a9f60b0da8918e83fa20090158deefdd7c566
tony <wabslygzj@163.com>
2019-08-03 16:34:19 +0800

    spark test file

--- src/main/java/com/tony/hbase/spark/SparkTest.scala
+++ src/main/java/com/tony/hbase/spark/SparkTest.scala
@@ -12,11 +12,22 @@ object SparkTest {
     val conf = new SparkConf()
       .setAppName("spark-test")
       .set("spark.cores.max", "2")
+      //本机运行,local[2]
       .setMaster("local[2]")
+      //设置jar
+//      .setJars(Seq())
+      //设置spark home
+//      .setSparkHome("/home/tony/spark/")
+    //配置线程池
+    //.setExecutorEnv()
     val sc = new SparkContext(conf)
     val text = sc.textFile("hdfs://localhost:9000/test1/test.txt")
     val result = text.flatMap(_.split(",")).map((_, 1)).reduceByKey(_ + _).collect()
+    //查询交易记录文件
+    //将交易记录文件
+//    用户交易消费信息挖掘
+    //用户浏览商品类型  用户浏览次数 商品的类型 top5
+    //推荐商品
     result.foreach(println)
   }
-
 }
