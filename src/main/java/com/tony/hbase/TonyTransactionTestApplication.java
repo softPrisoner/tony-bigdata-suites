494a4aca043f4094e18c591c1fe65e2f9aeb90cc
tony <wabslygzj@163.com>
2019-08-03 16:31:14 +0800

    commit

---
+++ src/main/java/com/tony/hbase/TonyTransactionTestApplication.java
@@ -0,0 +1,16 @@
+package com.tony.hbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tony
+ * @describe TonyTransationTestApplication
+ * @date 2019-08-03
+ */
+@SpringBootApplication
+public class TonyTransactionTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TonyTransactionTestApplication.class, args);
+    }
+}
