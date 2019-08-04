6d521b294f5c0545e1017802145054f98ab1c2b0
tony <wabslygzj@163.com>
2019-08-04 10:06:22 +0800

    commit

--- src/main/java/com/tony/hbase/TonyTransactionTestApplication.java
+++ src/main/java/com/tony/hbase/TonyTransactionTestApplication.java
@@ -2,13 +2,15 @@ package com.tony.hbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @author tony
- * @describe TonyTransationTestApplication
+ * @describe TonyTransactionTestApplication
  * @date 2019-08-03
  */
 @SpringBootApplication
+@EnableConfigurationProperties
 public class TonyTransactionTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TonyTransactionTestApplication.class, args);
