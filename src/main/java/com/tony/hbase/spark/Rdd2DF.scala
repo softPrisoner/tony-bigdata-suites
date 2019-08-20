6838da5ff714f04c25aa4ef6f3a1b3d7cdcae541
tony <wabslygzj@163.com>
2019-08-20 13:54:42 +0800

    rdd to df

---
+++ src/main/java/com/tony/hbase/spark/Rdd2DF.scala
@@ -0,0 +1,16 @@
+package com.tony.hbase
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @author tony
+  * @describe Rdd2DF
+  * @date 2019-08-20
+  */
+object Rdd2DF {
+  def rdd2DF(session: SparkSession, sourcePath: String): Unit = {
+    val rdd = session.sparkContext.textFile(sourcePath)
+    import session.implicits._
+    val df = rdd.toDF("line")
+  }
+}
